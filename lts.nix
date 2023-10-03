58f7c3280d079f4eb02a92f3c3ee9aab878e803b
Whovian9369 <Whovian9369@gmail.com>
2023-10-03 16:25:40 -0400

    Add placeholder `lts.nix` and `deps_lts.nix`

---
+++ lts.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  buildDotnetModule,
+  dotnetCorePackages,
+  fetchFromGitHub,
+}:
+
+buildDotnetModule {
+  pname = "Aaru";
+  version = "6.0.0-alpha9";
+  # version used is "v6.0.0-alpha9"
+
+  src = fetchFromGitHub {
+    owner = "aaru-dps";
+    repo = "Aaru";
+    rev = "89ae5e8d8b7aa47ca1e2573cf13ed37df5f34e8f";
+    hash = "sha256-cYvkCG7mc30RxHMeNvUvUov8ubtNmR4d6F2UkJmfHx0=";
+    fetchSubmodules = true;
+    leaveDotGit = false;
+  };
+
+  dotnet-sdk = dotnetCorePackages.sdk_7_0;
+  dotnet-runtime = dotnetCorePackages.runtime_7_0;
+  nugetDeps = ./deps_lts.nix;
+  projectFile = "Aaru/Aaru.csproj";
+  selfContainedBuild = false;
+  patches = [ ./remove_short_commit_hash.diff ];
+
+  meta = {
+    homepage = "https://aaru.app";
+    description = "The Pre-Release of a fully-featured media dump management solution";
+    license = [ lib.licenses.gpl3Only ];
+    mainProgram = "aaru";
+    maintainers = [ lib.maintainers.whovian9369 ];
+  };
+}
