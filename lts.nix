bfe2d328754571e79ff2a90611e9e8bd97945af9
Whovian9369 <Whovian9369@gmail.com>
2023-10-29 04:10:19 -0400

    Edited LTS derivation which sadly isn't working right now

--- lts.nix
+++ lts.nix
@@ -1,20 +1,27 @@
 {
   lib,
+  autoPatchelfHook,
   buildDotnetModule,
   dotnetCorePackages,
   fetchFromGitHub,
+  gcc,
+  stdenv,
 }:
 
+let
+  inherit (builtins) substring;
+  githash = "f4fef21d0d88b7931b95549782563db4da91a8f8";
+in
 buildDotnetModule {
   pname = "Aaru";
   version = "5.3.2";
-  # actual version used is "v6.0.0-alpha9"
+  # actual version used is "v5.3.2"
 
   src = fetchFromGitHub {
     owner = "aaru-dps";
     repo = "Aaru";
-    rev = "89ae5e8d8b7aa47ca1e2573cf13ed37df5f34e8f";
-    hash = "sha256-cYvkCG7mc30RxHMeNvUvUov8ubtNmR4d6F2UkJmfHx0=";
+    rev = githash;
+    hash = "sha256-f/8TXe02FXygmzHpf6h7mUdGmBq7pJS/ct9AeUg2lww=";
     fetchSubmodules = true;
     leaveDotGit = false;
   };
@@ -24,7 +31,20 @@ buildDotnetModule {
   nugetDeps = ./deps_lts.nix;
   projectFile = "Aaru/Aaru.csproj";
   selfContainedBuild = false;
-  # patches = [ ./remove_short_commit_hash.diff ];
+  runtimeId = "linux-x64";
+  nativeBuildInputs = [ autoPatchelfHook ];
+  buildInputs = [ stdenv.cc.cc gcc.cc.libgcc];
+  patchPhase = ''
+    substituteInPlace "Aaru/Aaru.csproj" \
+      "Aaru.Compression/Aaru.Compression.csproj" "Aaru.Core/Aaru.Core.csproj" \
+      "Aaru.Database/Aaru.Database.csproj" "Aaru.Devices/Aaru.Devices.csproj" \
+      "Aaru.Filesystems/Aaru.Filesystems.csproj" \
+      "Aaru.Filters/Aaru.Filters.csproj" "Aaru.Images/Aaru.Images.csproj" \
+      "Aaru.Partitions/Aaru.Partitions.csproj" \
+      "Aaru.Settings/Aaru.Settings.csproj" \
+      "Aaru.Tests.Devices/Aaru.Tests.Devices.csproj" --replace '{chash:8}' \
+      "${substring 0 8 githash}"
+  '';
 
   meta = {
     homepage = "https://aaru.app";
