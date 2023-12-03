6c12900eb4ab92c087626f3b7c09d9b925e77a55
Whovian9369 <Whovian9369@gmail.com>
2023-12-03 01:38:20 -0500

    Change default program to `prerelease` since `lts` doesn't really work right now.

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "A fully-featured media dump management solution.";
 
   inputs = {
-    # The nixos-23.05 branch of the NixOS/nixpkgs repository on GitHub.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    # The nixos-unstable branch of the NixOS/nixpkgs repository on GitHub.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default = self.packages.x86_64-linux.lts;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.prerelease;
     packages.x86_64-linux.lts = nixpkgs.legacyPackages.x86_64-linux.callPackage ./lts.nix {};
     packages.x86_64-linux.prerelease = nixpkgs.legacyPackages.x86_64-linux.callPackage ./prerelease.nix {};
   };
