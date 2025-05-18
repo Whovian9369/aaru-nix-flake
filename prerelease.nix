1932c49a9dce3ab84dcf3f10092624b951cc43cf
Whovian9369 <Whovian9369@gmail.com>
2025-05-18 19:28:30 -0400

    Swap out how `.#prerelease` gets its commit tag for the `substituteInPlace`.

--- prerelease.nix
+++ prerelease.nix
@@ -8,9 +8,8 @@
 
 let
   inherit (builtins) substring;
-  githash = "89ae5e8d8b7aa47ca1e2573cf13ed37df5f34e8f";
 in
-buildDotnetModule {
+buildDotnetModule rec {
   pname = "Aaru";
   version = "6.0.0-alpha9";
   # actual version used is "v6.0.0-alpha9"
@@ -18,7 +17,7 @@ buildDotnetModule {
   src = fetchFromGitHub {
     owner = "aaru-dps";
     repo = "Aaru";
-    rev = githash;
+    rev = "89ae5e8d8b7aa47ca1e2573cf13ed37df5f34e8f";
     hash = "sha256-cYvkCG7mc30RxHMeNvUvUov8ubtNmR4d6F2UkJmfHx0=";
     fetchSubmodules = true;
     leaveDotGit = false;
@@ -59,7 +58,7 @@ buildDotnetModule {
       "Aaru.Partitions/Aaru.Partitions.csproj" \
       "Aaru.Settings/Aaru.Settings.csproj" \
       "Aaru.Tests.Devices/Aaru.Tests.Devices.csproj" \
-      --replace-fail '{chash:8}' "${substring 0 8 githash}"
+      --replace-fail '{chash:8}' "${substring 0 8 src.rev}"
   '';
   # substituteStream(): WARNING: '--replace' is deprecated, use --replace-{fail,warn,quiet}.
 
