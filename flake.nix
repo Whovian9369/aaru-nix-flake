b9efd07aecf78396e5f5ba70bb77e0de082d70f0
Whovian9369 <Whovian9369@gmail.com>
2025-10-10 18:25:01 -0400

    Undeprecate and update `lts`.
    I've been sitting on these `lts` changes for a while, oops!
    This updates to LTS 5.4.1, which the developer of Aaru was gracious
    enough to put together at my request so we weren't using a completely
    EOL .NET SDK.

--- flake.nix
+++ flake.nix
@@ -9,26 +9,17 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      dotnet7-pkgs = import nixpkgs {
+      pkgs = import nixpkgs {
         inherit system;
-        config.allowUnfree = true;
-        config.permittedInsecurePackages = [
-          "dotnet-runtime-wrapped-7.0.20"
-          "dotnet-runtime-7.0.20"
-          "dotnet-sdk-wrapped-7.0.410"
-          "dotnet-sdk-7.0.410"
-        ];
       };
     in
   {
     packages = {
       x86_64-linux = {
         default = self.packages.${system}.prerelease;
-        git = nixpkgs.legacyPackages.${system}.callPackage ./git.nix {};
-        # lts = dotnet7-pkgs.callPackage ./lts.nix {};
-          # As of 2025-01-26, this build seems to be horribly broken beyond what I am able to fix myself.
-          # If you can, please feel free to open a PR!
-        prerelease = dotnet7-pkgs.callPackage ./prerelease.nix {};
+        git = pkgs.callPackage ./git.nix {};
+        lts = pkgs.callPackage ./lts.nix {};
+        prerelease = pkgs.callPackage ./prerelease.nix {};
       };
     };
   };
--- lts.nix
+++ lts.nix
@@ -5,48 +5,34 @@
   fetchFromGitHub,
 }:
 
-let
-  inherit (builtins) substring;
-in
 buildDotnetModule rec {
   pname = "Aaru";
-  version = "5.3.2";
-  # actual version used is "v5.3.2"
+  version = "5.4.1";
+  # actual version used is "v5.4.1"
 
   src = fetchFromGitHub {
     owner = "aaru-dps";
     repo = "Aaru";
-    rev = "f4fef21d0d88b7931b95549782563db4da91a8f8";
-    hash = "sha256-f/8TXe02FXygmzHpf6h7mUdGmBq7pJS/ct9AeUg2lww=";
+    rev = "06405f4976e44afcae20b9681ae367454829551a";
+    hash = "sha256-Xbo9/UeKa9E/lp/dbnPsXBapT5v04+cOyGICO//oZfY=";
     fetchSubmodules = true;
     leaveDotGit = false;
   };
 
-  dotnet-sdk = dotnetCorePackages.sdk_7_0;
-  dotnet-runtime = dotnetCorePackages.runtime_7_0;
+  buildType = "Debug";
+
+  dotnet-sdk = dotnetCorePackages.sdk_8_0;
+  dotnet-runtime = dotnetCorePackages.runtime_8_0;
   nugetDeps = ./deps_lts.json;
   projectFile = "Aaru/Aaru.csproj";
 
-  dotnetBuildFlags = [ "--framework net7.0" ];
-  dotnetInstallFlags = [ "--framework net7.0" ];
+  dotnetBuildFlags = [ "--framework net8.0" ];
+  dotnetInstallFlags = [ "--framework net8.0" ];
   selfContainedBuild = false;
   # runtimeId = "linux-x64";
   executables = [ "aaru" ];
 
-  patches = [
-    ./patches/lts/chash_to_shortrev.diff
-      # Swap out uses of "git" with shortened version of src.rev
-      # Used in "aaru --version" and during build.
-
-    # ./patches/lts/build_with_net7.diff
-      # Make this build via NET 7.0 instead of netcoreapp3.1
-
-    # ./patches/lts/fix_net7_build.diff
-      # Fix build on Net 7.0 by upgrading Microsoft.EntityFrameworkCore related
-      # entries
-  ];
-
-  patchPhase = ''
+  postPatch = ''
     # Swap out uses of "git" with shortened version of src.rev
     # Used in "aaru --version" and during build.
     substituteInPlace \
@@ -68,29 +54,7 @@ buildDotnetModule rec {
       "Aaru.Partitions/Aaru.Partitions.csproj" \
       "Aaru.Settings/Aaru.Settings.csproj" \
       "Aaru.Tests.Devices/Aaru.Tests.Devices.csproj" \
-      --replace-fail '{chash:8}' "${substring 0 8 src.rev}"
-    # Make this build via NET 7.0 instead of netcoreapp3.1
-    substituteInPlace \
-      "Aaru/Aaru.csproj" \
-      "Aaru.Checksums/Aaru.Checksums.csproj" \
-      "Aaru.CommonTypes/Aaru.CommonTypes.csproj" \
-      "Aaru.Compression/Aaru.Compression.csproj" \
-      "Aaru.Console/Aaru.Console.csproj" \
-      "Aaru.Core/Aaru.Core.csproj" \
-      "Aaru.Database/Aaru.Database.csproj" \
-      "Aaru.Decoders/Aaru.Decoders.csproj" \
-      "Aaru.Decryption/Aaru.Decryption.csproj" \
-      "Aaru.Devices/Aaru.Devices.csproj" \
-      "Aaru.Dto/Aaru.Dto.csproj" \
-      "Aaru.Filesystems/Aaru.Filesystems.csproj" \
-      "Aaru.Filters/Aaru.Filters.csproj" \
-      "Aaru.Helpers/Aaru.Helpers.csproj" \
-      "Aaru.Images/Aaru.Images.csproj" \
-      "Aaru.Partitions/Aaru.Partitions.csproj" \
-      "Aaru.Settings/Aaru.Settings.csproj" \
-      "Aaru.Tests/Aaru.Tests.csproj" \
-      "Aaru.Tests.Devices/Aaru.Tests.Devices.csproj" \
-      --replace-fail "netcoreapp3.1" "net7.0"
+      --replace-fail '{chash:8}' "${lib.substring 0 8 src.rev}"
   '';
 
   meta = {
